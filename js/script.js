1b30238aa1ec20697bf4dbf740007819c136dc9d
Fernando-Castro-Hernandez <jesus.castro@tecdesoftware.edu.mx>
2025-11-18 08:50:48 -0600

    Añadimos carpetas al main

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let contador = 1
+function cambiarImagen() {
+    contador++;
+    const imagen = document.getElementById("banner")
+    imagen.style.opacity = 0
+    setTimeout(() =>{
+        imagen.src = `https://picsun.photos/800/400?random=${contador}`;
+        imagen.style.opacity = 1;
+    }, 300);
+}
